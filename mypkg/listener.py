02d4b5e3495d7e25da9076eaa3389002ccf19a1c
kouzou2111 <byopo@bangban.uk>
2023-12-29 14:40:46 +0900

    lis

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -2,11 +2,14 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
+class Lstener(Node):
+    def __init__(self):
+        super().__init__("listener")
+        self.pub = node.create_publisher(Int16, "counyup", self.cb, 10)
+    
+    def cb(self, msg):
+        self.get_logger().info("Listen:%d"%msg.data)
 
 rclpy.init()
-node = Node("listener")
-sub = node.create_subscription(Int16, "countup", cb, 10)
+node = listener
 rclpy.spin(node)
